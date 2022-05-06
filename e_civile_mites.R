66152cd6cef970d9b26b8fb8ae441b7cf3de4dce
Nancy McIntyre <nancy.mcintyre@ttu.edu>
2022-05-06 16:33:47 -0500

    Add files via upload

---
+++ e_civile_mites.R
@@ -0,0 +1,65 @@
+##############################
+# Code for water mites data
+############################## 
+# 
+# Objective:
+# 
+# Analyzing the prevalence and intensity of water mites on E. civile across landcover context and host sex. 
+# 
+# For landcover context, host sex, and year's sampled, subset files were generated. 
+#
+# Intensity:
+# 
+# We used bias-corrected and accelerated bootstrap for confidence intervals regarding water mites.
+# For statistical significance testing, we used a Wilcox test and a bootstrapped t-test for water mite intensity.
+# 
+# Prevalence:
+# 
+# We used a Clopper-Pearson confidence interval, which takes into account the binary nature of prevalence data
+# A Fischer's Exact test was used to determine any statistically significant difference between water mite prevalence and predictors.
+# 
+
+library(GenBinomApps)
+library(coxed)
+library(MKinfer)
+library(ape)
+
+getwd()
+water.mites.df <- read.csv("C:/yourdirectory")
+
+# Water mite intensity
+
+mean(water.mites.df$Mites.) # Mean intensity of water mites on E. civile 
+sd(water.mites.df$Mites.) # Standard deviation of water mites on E. civile
+
+# Use mean and sd to determine the bias-corrected and accelerated bootstrap
+theta <- rnorm(1000, mean, sd)
+bca(theta, conf.level = 0.95) 
+
+# Wilcox test to quantify the intensity of water mites by a predictor (e.g., landuse, host sex)
+
+wilcox.test(Mites. ~ as.factor(Landuse), data = water.mites.df, exact = TRUE)
+# W = 2364, p-value = 0.3528
+
+wilcox.test(Mites. ~ as.factor(Sex), data = water.mites.df, exact = TRUE)
+
+# Bootstrapped t-test for water mite intensity by a predictor (e.g., landuse, host sex)
+
+boot.t.test(Mites. ~ as.factor(Landuse), data = intensit)
+
+boot.t.test(Mites. ~ as.factor(Sex), data = mites)
+
+# Water mite prevalence
+
+length(which(water.mites.df$Mites == 1))/nrow(water.mites.df) * 100 # Prevalence of water mites on E. civile
+
+
+clopper.pearson.ci(k = 39, # k is the # of failures and/or successes (e.g., parasitized or not)
+                   n = 100000, alpha = 0.05,
+                   CI = "two.sided")
+
+# Fischer's exact test to quantify the prevalence of water mites by a predictor (e.g., landuse, host sex)
+
+fisher.test(water.mites.df$Landuse, water.mites.df$Mites)
+
+fisher.test(water.mites.df$Sex, water.mites.df$Mites)
