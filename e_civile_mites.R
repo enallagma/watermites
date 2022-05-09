93347d3746225bf240ad967a5c227087fa450797
Nancy McIntyre <nancy.mcintyre@ttu.edu>
2022-05-09 06:51:03 -0500

    Update e_civile_mites.R

--- e_civile_mites.R
+++ e_civile_mites.R
@@ -4,9 +4,9 @@
 # 
 # Objective:
 # 
-# Analyze the prevalence and intensity of water mites on Enallagma civile across landcover context and host sex. 
+# Analyze the prevalence and intensity of water mites on Enallagma civile across land-use context and host sex. 
 # 
-# For landcover context, host sex, and years sampled, subset files were generated. 
+# For land-use context, host sex, and years sampled, subset files were generated. 
 #
 # Intensity:
 # 
@@ -16,8 +16,12 @@
 # Prevalence:
 # 
 # We used a Clopper-Pearson confidence interval, which takes into account the binary nature of prevalence data
-# A Fischer's Exact test was used to determine any statistically significant difference between water mite prevalence and predictors.
+# A Fisher's exact test was used to determine any statistically significant difference between water mite prevalence and predictors.
 # 
+# Spatial autocorrelation:
+#
+# We calculated Moran's I to test the null hypothesis of no spatial autocorrelation in the number of mites present based on site proximity.
+#
 
 library(GenBinomApps)
 library(coxed)
@@ -32,10 +36,10 @@ water.mites.df <- read.csv("C:/yourdirectory/e_civile_mites.csv")
 # grass <- subset(water.mites.df, Landuse == "Grassland")
 # View(grass)
 
-# Water mite intensity
+# Water mite intensity over all sampling dates
 
-mean(water.mites.df$Mites.) # Mean intensity of water mites on E. civile 
-sd(water.mites.df$Mites.) # Standard deviation of water mites on E. civile
+mean(water.mites.df$MitesNum) # Mean intensity of water mites on E. civile 
+sd(water.mites.df$MitesNum) # Standard deviation of water mites on E. civile
 
 # Use mean and sd to determine the bias-corrected and accelerated bootstrap
 theta <- rnorm(1000, mean, sd)
@@ -43,20 +47,19 @@ bca(theta, conf.level = 0.95)
 
 # Wilcox test to quantify the intensity of water mites by a predictor (e.g., landuse, host sex)
 
-wilcox.test(Mites. ~ as.factor(Landuse), data = water.mites.df, exact = TRUE)
+wilcox.test(MitesNum ~ as.factor(Landuse), data = water.mites.df, exact = TRUE)
 
-wilcox.test(Mites. ~ as.factor(Sex), data = water.mites.df, exact = TRUE)
+wilcox.test(MitesNum ~ as.factor(Sex), data = water.mites.df, exact = TRUE)
 
 # Bootstrapped t-test for water mite intensity by a predictor (e.g., landuse, host sex)
 
-boot.t.test(Mites. ~ as.factor(Landuse), data = intensit)
+boot.t.test(MitesNum ~ as.factor(Landuse), data = intensit)
 
-boot.t.test(Mites. ~ as.factor(Sex), data = mites)
+boot.t.test(MitesNum ~ as.factor(Sex), data = mites)
 
-# Water mite prevalence
+# Water mite prevalence over all sampling dates
 
 length(which(water.mites.df$Mites == 1))/nrow(water.mites.df) * 100 # Prevalence of water mites on E. civile
-
 
 clopper.pearson.ci(k = 39, # k is the # of failures and/or successes (e.g., parasitized or not)
                    n = 100000, alpha = 0.05,
@@ -78,4 +81,8 @@ water.mites.inv <- 1/mites.dists
 water.mites.inv[!is.finite(water.mites.inv)] <- 0
 diag(water.mites.inv) <- 0
 
-Moran.I(water.mites.df$Mites., water.mites.inv)
+# Intensity:
+Moran.I(water.mites.df$MitesNum, water.mites.inv)
+
+# Prevalence:
+Moran.I(water.mites.df$MitesPres, water.mites.inv)
