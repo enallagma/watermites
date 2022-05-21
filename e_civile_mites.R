4cb66ad7d3f427122079075a2dcfdad9a4a79857
Nancy McIntyre <nancy.mcintyre@ttu.edu>
2022-05-20 21:18:29 -0500

    Update e_civile_mites.R

--- e_civile_mites.R
+++ e_civile_mites.R
@@ -41,7 +41,7 @@ water.mites.df <- read.csv("C:/yourdirectory/e_civile_mites.csv")
 # To ignore 0's in MitesNum column (so as to calculate true intensity of infection rather than mite abundance):
 is.na(water.mites.df[[10]]) <- water.mites.df[[10]] < 1
 
-# Subsets:
+# Year subsets:
 Yr2006 <- subset(water.mites.df, Year == "2006")
 Yr2007 <- subset(water.mites.df, Year == "2007")
 
@@ -53,12 +53,32 @@ Yr2007 <- subset(water.mites.df, Year == "2007")
 boot.t.test(Yr2006$MitesNum ~ as.factor(Yr2006$Landuse), na.rm=TRUE)
 boot.t.test(Yr2006$MitesNum ~ as.factor(Yr2006$Sex), na.rm=TRUE)
 
-mean06 <- mean(Yr2006$MitesNum, na.rm=TRUE) # Mean intensity of water mites on E. civile 
-sd06 <- sd(Yr2006$MitesNum, na.rm=TRUE) # Standard deviation of water mites on E. civile
+# Subsets for confidence intervals for intensity by landuse, sex by year:
+C06 <- subset(Yr2006, Landuse == "Cropland")
+G06 <- subset(Yr2006, Landuse == "Grassland")
+M06 <- subset(Yr2006, Sex == "M")
+F06 <- subset(Yr2006, Sex == "F")
 
 # Use mean and sd to determine the bias-corrected and accelerated bootstrap 95% CI:
-theta06 <- rnorm(1000, mean06, sd06)
-bca(theta06, conf.level = 0.95) 
+meanC06 <- mean(C06$MitesNum, na.rm=TRUE)  
+sdC06 <- sd(C06$MitesNum, na.rm=TRUE) 
+thetaC06 <- rnorm(1000, meanC06, sdC06)
+bca(thetaC06, conf.level = 0.95) # cropland CI
+
+meanG06 <- mean(G06$MitesNum, na.rm=TRUE)  
+sdG06 <- sd(G06$MitesNum, na.rm=TRUE) 
+thetaG06 <- rnorm(1000, meanG06, sdG06)
+bca(thetaG06, conf.level = 0.95) # grassland CI
+
+meanM06 <- mean(M06$MitesNum, na.rm=TRUE)  
+sdM06 <- sd(M06$MitesNum, na.rm=TRUE) 
+thetaM06 <- rnorm(1000, meanM06, sdM06)
+bca(thetaM06, conf.level = 0.95) # male CI
+
+meanF06 <- mean(F06$MitesNum, na.rm=TRUE)  
+sdF06 <- sd(F06$MitesNum, na.rm=TRUE) 
+thetaF06 <- rnorm(1000, meanF06, sdF06)
+bca(thetaF06, conf.level = 0.95) # female CI 
 
 # Water mite prevalence:
 
@@ -78,12 +98,31 @@ clopper.pearson.ci(16, 48, alpha=0.05)
 boot.t.test(Yr2007$MitesNum ~ as.factor(Yr2007$Landuse), na.rm=TRUE)
 boot.t.test(Yr2007$MitesNum ~ as.factor(Yr2007$Sex), na.rm=TRUE)
 
-mean07 <- mean(Yr2007$MitesNum, na.rm=TRUE) # Mean intensity of water mites on E. civile 
-sd07 <- sd(Yr2007$MitesNum, na.rm=TRUE) # Standard deviation of water mites on E. civile
+C07 <- subset(Yr2007, Landuse == "Cropland")
+G07 <- subset(Yr2007, Landuse == "Grassland")
+M07 <- subset(Yr2007, Sex == "M")
+F07 <- subset(Yr2007, Sex == "F")
 
 # Use mean and sd to determine the bias-corrected and accelerated bootstrap 95% CI:
-theta07 <- rnorm(1000, mean07, sd07)
-bca(theta07, conf.level = 0.95) 
+meanC07 <- mean(C07$MitesNum, na.rm=TRUE)  
+sdC07 <- sd(C07$MitesNum, na.rm=TRUE) 
+thetaC07 <- rnorm(1000, meanC07, sdC07)
+bca(thetaC07, conf.level = 0.95) # cropland CI
+
+meanG07 <- mean(G07$MitesNum, na.rm=TRUE)  
+sdG07 <- sd(G07$MitesNum, na.rm=TRUE) 
+thetaG07 <- rnorm(1000, meanG07, sdG07)
+bca(thetaG07, conf.level = 0.95) # grassland CI
+
+meanM07 <- mean(M07$MitesNum, na.rm=TRUE)  
+sdM07 <- sd(M07$MitesNum, na.rm=TRUE) 
+thetaM07 <- rnorm(1000, meanM07, sdM07)
+bca(thetaM07, conf.level = 0.95) # male CI
+
+meanF07 <- mean(F07$MitesNum, na.rm=TRUE)  
+sdF07 <- sd(F07$MitesNum, na.rm=TRUE) 
+thetaF07 <- rnorm(1000, meanF07, sdF07)
+bca(thetaF07, conf.level = 0.95) # female CI 
 
 # Water mite prevalence:
 
